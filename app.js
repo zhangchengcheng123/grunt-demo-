02f0824993bfff845033751bac3408fe6d8ee17d
zhangchengcheng123 <1306073684@qq.com>
2019-03-12 14:47:42 +0800

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const add=require('./sum');
-var x=process.argv[2]||1,
-    y=process.argv[3]||1;
-//console.log(`${x}+${y}=${Number(x)+Number(y)}`);
-console.log(`${x}+${y}=${add(x,y)}`)
