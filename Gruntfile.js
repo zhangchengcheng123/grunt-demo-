0a4398dd1bd2b4d7a50b67c35d3774bd126c7df3
wangding <408542507@qq.com>
2019-03-12 16:28:13 +0800

    imagesmin

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    imagemin:{
+     dynamic:{
+      files:[{
+        expand: true,
+        cwd: './images',
+        src: ['**/*.{png,jpg,gif}'],
+        dest: 'dist/'
+      }]
+     }   
+    }
+  });
+
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+    grunt.registerTask('default', ['imagemin']);    
+
+};
